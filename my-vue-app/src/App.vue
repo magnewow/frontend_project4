d3185d19f84331f052abaf582e2c71eb0b735f7f
magnewow <116713718+magnewow@users.noreply.github.com>
2026-09-24 16:21:06 +0800

    Add files via upload

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Форма регистрации</h1>
+    
+    <input v-model="name" placeholder="Ваше имя" />
+    <input v-model="email" placeholder="Email" />
+    
+    <button @click="showData">Показать данные</button>
+    
+    <label>
+      <input type="checkbox" v-model="showMessage" />
+      Показать сообщение
+    </label>
+    <p v-if="showMessage">Добро пожаловать, {{ name }}!</p>
+    
+    <h3>Любимый язык программирования:</h3>
+    <label>
+      <input type="radio" value="JavaScript" v-model="favoriteLang" />
+      JavaScript
+    </label>
+    <label>
+      <input type="radio" value="Python" v-model="favoriteLang" />
+      Python
+    </label>
+    <label>
+      <input type="radio" value="Java" v-model="favoriteLang" />
+      Java
+    </label>
+    <p>Вы выбрали: {{ favoriteLang }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('')
+const email = ref('')
+const showMessage = ref(false)
+const favoriteLang = ref('JavaScript')
+
+const showData = () => {
+  console.log('Имя:', name.value)
+  console.log('Email:', email.value)
+}
+</script>
